90ad9c7b3601bd4328c1dc3509ed97a46eab9e24
Jundev <junhyeong3289@gmail.com>
2021-03-24 18:45:13 +0900

    add init scrren and function

--- App.js
+++ App.js
@@ -1,8 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Alert, View, BackHandler, LogBox, Keyboard, Text, StyleSheet } from "react-native";
+import { useGlobal, useDispatch } from "reactn";
 
 export default function App() {
+  const [, setKeyboardVisible] = useGlobal("keyboard");
+  React.useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener("keyboardDidShow", () => {
+      setKeyboardVisible(true); // or some other action
+    });
+    const keyboardDidHideListener = Keyboard.addListener("keyboardDidHide", () => {
+      setKeyboardVisible(false); // or some other action
+    });
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
+
+  React.useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", () => {
+      Alert.alert(
+        "앱종료",
+        "앱을 종료하시겠습니까??",
+        [
+          { text: "취소", onPress: () => {}, style: "cancel" },
+          {
+            text: "종료",
+            onPress: async () => {
+              localData.isExit = true;
+              BackHandler.exitApp();
+            },
+          },
+        ],
+        { cancelable: false }
+      );
+      return true;
+    });
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
